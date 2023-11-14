62d44b9c5fd034691a3380296f3bc4137e4dd8b1
Worden.a <Worden.a@308-PC10>
2023-11-14 12:55:22 +0000

    Add project files.

---
+++ Unit21-Assignment23/assets/bootstrap/js/sessionStorage.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    var page = sessionStorage.getItem('page');
+    var navbar = document.getElementsByClassName('navbar-nav me-auto')[0];
+    if (page == null) {
+        $(navbar[0]).addClass('nav-link active link-light');
+    }
+    else {
+        navbar.forEach((item, index) => {
+            if (item.children[0].href == page) {
+                $(item).addClass('nav-link active link-light').siblings().removeClass('nav-link active link-light');
+            }
+        });
+    }
+ })
+
+$(document).on('click', 'ul li', function () {
+    var x = this.children[0].href;
+    sessionStorage.setItem('page', x);
+});

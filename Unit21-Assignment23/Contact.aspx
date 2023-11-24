47738b04fadb822a6a49139a31b7190c87a735ad
Blaadam <Adam.Worden06@gmail.com>
2023-11-24 14:24:54 +0000

    Contact Form

--- Unit21-Assignment23/Contact.aspx
+++ Unit21-Assignment23/Contact.aspx
@@ -1,5 +1,38 @@
 ﻿<%@ Page Title="Contact" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="Unit21_Assignment23.Contact" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p class="text-primary" style="text-align: center; font-weight: bold; font-size: 26px; margin-top: 8px; margin-bottom: 8px; width: 100%; padding-right: 0px; margin-right: 16px; margin-left: 0px;">Contact Us</p>
+    <section class="position-relative py-4 py-xl-5" style="margin-top: -23px; height: 94vh">
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-6 col-xl-4">
+                    <div class="card mb-5">
+                        <div class="card-body d-flex flex-column align-items-center">
+                            <form class="text-center" method="post">
+                                <!-- User Details -->
+                                <div class="mb-3">
+                                    <asp:TextBox ID="tbName" runat="server" class="form-control" type="username" placeholder="Name"></asp:TextBox>
+                                </div>
+
+                                <div class="mb-3">
+                                    <asp:TextBox ID="tbEmail" runat="server" class="form-control" type="email" placeholder="Email"></asp:TextBox>
+                                </div>
+
+                                <div class="mb-3">
+                                    <asp:TextBox ID="tbMessage" runat="server" class="form-control" type="message" placeholder="Message"></asp:TextBox>
+                                </div>
+                                
+                                <!-- Control Button -->
+                                <div class="mb-3">
+                                    <asp:Button ID="btnSubmit" runat="server" Text="Submit" class="btn btn-primary d-block w-100" type="submit" OnClick="btnSubmit_Click" />
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </asp:Content>
--- Unit21-Assignment23/Contact.aspx.cs
+++ Unit21-Assignment23/Contact.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,61 @@ namespace Unit21_Assignment23
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            tbName.Focus();
+            this.Form.DefaultButton = btnSubmit.UniqueID;
+        }
 
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            const string SQLStringConnection = "18worden.a@thestudio-liverpool.netConnectionString";
+            try
+            {
+                // Check if data on the page is able to be submitted
+                if (!Page.IsValid)
+                {
+                    return;
+                }
+
+                // Create a connection string
+                SqlConnection MainConnection = new SqlConnection(ConfigurationManager.ConnectionStrings[SQLStringConnection].ConnectionString);
+
+                MainConnection.Open();
+
+                // Create a query to submit data into
+                string InsertQuery = @"INSERT INTO Contact_Table(Name, Email, Message) values(@Name, @Email, @Message)";
+
+                SqlCommand InsertQueryCommand = new SqlCommand(InsertQuery, MainConnection);
+                InsertQueryCommand.Parameters.AddWithValue("@Name", tbName.Text);
+                InsertQueryCommand.Parameters.AddWithValue("@Email", tbEmail.Text);
+
+                string ContactString = "";
+
+                // If the entered message is greater than 125 characters - trim the end
+                if (tbMessage.Text.Length > 125)
+                {
+                    ContactString = tbMessage.Text.Remove(125);
+                }
+                else
+                {
+                    ContactString = tbMessage.Text;
+                }
+                InsertQueryCommand.Parameters.AddWithValue("@Message", ContactString);
+
+                // Execute the query - without expecting a result
+                InsertQueryCommand.ExecuteNonQuery();
+
+                // Close the connection to optimise the Database
+                MainConnection.Close();
+
+                // Thank the client and redirect them to the home page
+                Response.Write("Thank you for your submission");
+                Response.Redirect("Home.aspx");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("" + ex.ToString());
+                //throw;
+            }
         }
     }
 }
